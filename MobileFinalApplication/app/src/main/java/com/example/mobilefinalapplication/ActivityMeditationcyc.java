bcdb59fd67f564c75808200449ac096ca69e6243
ChenYichan <dmt1709086@xmu.edu.my>
2020-06-23 11:59:36 +0800

    cyc2

---
+++ MobileFinalApplication/app/src/main/java/com/example/mobilefinalapplication/ActivityMeditationcyc.java
@@ -0,0 +1,53 @@
+package com.example.mobilefinalapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+public class ActivityMeditationcyc extends AppCompatActivity {
+
+    ImageView clickImagecyc;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meditationcyc);
+        clickImagecyc= (ImageView) findViewById(R.id.cycmeditationseries1);
+        clickImagecyc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(
+
+                        ActivityMeditationcyc.this,R.style.BottomSheet
+                );
+                View bottomSheet= LayoutInflater.from(getApplicationContext()).
+                        inflate(R.layout.meditation_subpagecyc,(LinearLayout) findViewById(R.id.bottomlogcyc));
+                bottomSheet.findViewById(R.id.subpagedialogcyc).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        openSubMeditationcyc();
+                    }
+                });
+
+                bottomSheetDialog.setContentView(bottomSheet);
+                bottomSheetDialog.show();
+
+
+            }
+        });
+
+    }
+
+    public void openSubMeditationcyc()
+    {
+        Intent intent=new Intent(this,MeditationSubcyc.class);
+        startActivity(intent);
+    }
+}
---
+++ MobileFinalApplication/app/src/main/java/com/example/mobilefinalapplication/MeditationSubcyc.java
@@ -0,0 +1,23 @@
+package com.example.mobilefinalapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+public class MeditationSubcyc extends AppCompatActivity {
+
+   // ImageView clickImagecyc;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meditation_subcyc);
+
+    }
+}
