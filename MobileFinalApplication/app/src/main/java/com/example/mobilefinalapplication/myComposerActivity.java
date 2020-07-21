b574525ec7780bc18060692011003c2203711b91
Camille7777 <44392324+Camille7777@users.noreply.github.com>
2020-07-22 01:53:09 +0800

    Composer Updated 3.0

--- MobileFinalApplication/app/src/main/java/com/example/mobilefinalapplication/myComposerActivity.java
+++ MobileFinalApplication/app/src/main/java/com/example/mobilefinalapplication/myComposerActivity.java
@@ -1,16 +1,32 @@
 package com.example.mobilefinalapplication;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class myComposerActivity extends AppCompatActivity {
 
+    Button backComposer;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_composer);
+        backComposer = findViewById(R.id.back_zyt);
+
+        backComposer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(myComposerActivity.this , ComposerMain.class);
+
+                startActivity(i);
+            }
+        });
     }
 
 }
