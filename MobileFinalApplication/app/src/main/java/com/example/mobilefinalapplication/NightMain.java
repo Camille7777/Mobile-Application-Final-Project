f1db8a473472f9503459edf42746ee3a9bf2c7e1
plusru <994837979@qq.com>
2020-07-22 12:54:12 +0800

    nighttalk change

--- MobileFinalApplication/app/src/main/java/com/example/mobilefinalapplication/NightMain.java
+++ MobileFinalApplication/app/src/main/java/com/example/mobilefinalapplication/NightMain.java
@@ -3,9 +3,15 @@ package com.example.mobilefinalapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class NightMain extends AppCompatActivity {
     Button bar_icon1;
@@ -13,6 +19,14 @@ public class NightMain extends AppCompatActivity {
     Button bar_icon3;
     Button bar_icon4;
     Button bar_icon5;
+    Button email_rz;
+    LottieAnimationView email;
+    TextView text_rz;
+    TextView text2_rz;
+    ImageView icon_rz;
+
+
+    Animation atg, btgone, btgtwo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -24,6 +38,36 @@ public class NightMain extends AppCompatActivity {
         bar_icon3 =findViewById(R.id.bar_icon3);
         bar_icon4 =findViewById(R.id.bar_icon4);
         bar_icon5 =findViewById(R.id.bar_icon5);
+        email_rz=findViewById(R.id.email_rz);
+        email=findViewById(R.id.email);
+
+        text_rz=findViewById(R.id.text_rz);
+        text2_rz=findViewById(R.id.text2_rz);
+        icon_rz=findViewById(R.id.icon_rz);
+
+        atg = AnimationUtils.loadAnimation(this, R.anim.atg);
+        btgone = AnimationUtils.loadAnimation(this, R.anim.btgone);
+        btgtwo = AnimationUtils.loadAnimation(this, R.anim.btgtwo);
+
+
+        text_rz.startAnimation(atg);
+        text2_rz.startAnimation(btgone);
+        icon_rz.startAnimation(btgone);
+        email_rz.startAnimation(btgtwo);
+
+        email.setAlpha((float) 0.0);
+
+        //load animations
+        email_rz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                email.setAlpha((float) 1.0);
+                email.animate().alpha(1).translationY(-80).setDuration(300).start();
+                email.playAnimation();
+            }
+        });
+
+
 
         bar_icon1.setOnClickListener(new View.OnClickListener() {
             @Override
