4fd9b48e72c724e04872d2297da43b260465f186
tulipblack <44391779+tulipblack@users.noreply.github.com>
2020-07-26 10:53:56 +0800

    synchronize name

--- MobileFinalApplication/app/src/main/java/com/example/mobilefinalapplication/MyPageMain.java
+++ MobileFinalApplication/app/src/main/java/com/example/mobilefinalapplication/MyPageMain.java
@@ -1,11 +1,47 @@
 package com.example.mobilefinalapplication;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MyPageMain extends AppCompatActivity {
     Button bar_icon1;
@@ -16,6 +52,14 @@ public class MyPageMain extends AppCompatActivity {
 
     private SessionHandler session;
     Button logout;
+
+    //edit password
+    TextView username;
+    TextView useremail;
+    private static final String KEY_STATUS = "status";
+    private static final String KEY_MESSAGE = "message";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -82,7 +126,16 @@ public class MyPageMain extends AppCompatActivity {
                 startActivity(a);
             }
         });
-    }
 
+        //syncronize the username
+        session = new SessionHandler(getApplicationContext());
+
+        username = findViewById(R.id.username_m);
+        useremail = findViewById(R.id.emailvalue_m);
+        User user = session.getUserDetails();
+        username.setText(user.getUsername());
+        useremail.setText(user.getFullName());
+
+    }
 
 }
